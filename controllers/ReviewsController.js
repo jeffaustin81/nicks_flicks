7c7d7ae9eeb2c57d5feac278e52f96c1e0db15f4
Jeff Austin <jeffaustin81@gmail.com>
2015-10-02 10:23:41 -0700

    Add reviews controller, Edit reviews factory

---
+++ controllers/ReviewsController.js
@@ -0,0 +1,4 @@
+nicksFlicks.controller('ReviewsCtrl', function ReviewsCtrl($scope, ReviewsFactory) {
+  $scope.reviews = ReviewsFactory.reviews;
+  $scope.ReviewsFactory = ReviewsFactory;
+});
--- services/ReviewsFactory.js
+++ services/ReviewsFactory.js
@@ -1,10 +1,38 @@
 nicksFlicks.factory('ReviewsFactory', function ReviewsFactory() {
-  var factory = {};
-  factory.reviews = [];
-  factory.addReview = function() {
-    factory.reviews.push({ title: factory.reviewTitle, id: factory.reviews.length + 1});
-    factory.reviewTitle = null;
-  };
+    var factory = {};
+    factory.reviews = [{
+        title: "",
+        review: "",
+        id: factory.reviews.length + 1
+    },
+    {
+        title: "",
+        review: "",
+        id: factory.reviews.length + 1
+    },
+    {
+        title: "",
+        review: "",
+        id: factory.reviews.length + 1
+    },
+    {
+        title: "",
+        review: "",
+        id: factory.reviews.length + 1
+    },
+    {
+        title: "",
+        review: "",
+        id: factory.reviews.length + 1
+    }];
+    factory.addReview = function() {
+        factory.reviews.push({
+            title: factory.reviewTitle,
+            review: factory.review,
+            id: factory.reviews.length + 1
+        });
+        factory.reviewTitle = null;
+    };
 
-  return factory;
+    return factory;
 });
